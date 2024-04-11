1a75b5bcd1a7e8c798dcece13acc744fa72a2746
JustKMP <a.chavarria@udea.edu.co>
2024-04-10 20:15:42 -0500

    Fith commit

---
+++ src/main/java/culturemedia/service/impl/CultureMediaImpl.java
@@ -0,0 +1,29 @@
+package culturemedia.service.impl;
+
+import java.util.List;
+
+import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.service.CultureMediaService;
+
+public class CultureMediaImpl implements CultureMediaService {
+
+    @Override
+    public List<Video> findAllVideos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllVideos'");
+    }
+
+    @Override
+    public Video add(Video video) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public View add(View view) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+}
